9a60c1be5ca7e7e929e94b27e0ac5ba95b073cd4
lenovo-manjaro <1774104802@qq.com>
2019-02-23 10:08:45 +0800

    Emacs更新个人配置

--- emacs/lisp/custom.el
+++ emacs/lisp/custom.el
@@ -10,8 +10,15 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
  '(package-selected-packages
    (quote
-    (auctex org company monokai-theme hungry-delete swiper counsel smartparens)))
+    (w3m auctex org company monokai-theme hungry-delete swiper counsel smartparens)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
+
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 181 :width normal)))))
 
