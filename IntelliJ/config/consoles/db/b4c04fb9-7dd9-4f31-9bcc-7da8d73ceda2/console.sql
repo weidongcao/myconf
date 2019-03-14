ca2baf51f1e12aa8df3e01a9dccdd98f5a2684f6
lenovo-manjaro <1774104802@qq.com>
2019-03-14 12:06:03 +0800

    IntelliJ(IDEA, Pycharm)系列工具配置文件

---
+++ IntelliJ/config/consoles/db/b4c04fb9-7dd9-4f31-9bcc-7da8d73ceda2/console.sql
@@ -0,0 +1,2 @@
+INSERT INTO wedo.runs (rdate, lengths, run_time, build_time, rmode, create_time, mark) VALUES ('2019-02-21', 5.1, 30, 40, 2, '2019-02-23 14:28:00', '大街+健身房');
+INSERT INTO wedo.runs (rdate, lengths, run_time, build_time, rmode, create_time, mark) VALUES ('2019-02-22', 6.5, 30, 90, 2, '2019-02-23 14:28:00', '大街+健身房');
