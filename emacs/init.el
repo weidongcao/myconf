e78a11ae56268a3d52161797ef4da69c7783fcb9
ubuntu <1774104802@qq.com>
2019-05-09 13:44:07 +0800

    Emacs使用org-mode管理配置文件

--- emacs/init.el
+++ emacs/init.el
@@ -1,23 +1,12 @@
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
+;; You may delete these explanatory s.
+ 
 (package-initialize)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-(defun open-my-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
+(require 'org-install)
+(require 'ob-tangle)
 
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-keybindings)
-(require 'init-better-defaults)
-(require 'org)
-
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
-(load-file custom-file)
- 
-
+(org-babel-load-file (expand-file-name "wedo.org" user-emacs-directory ))
