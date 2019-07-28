facfc04cd796bd8b86c7f6c9d25d07b0b6bf6a7d
ubuntu <1774104802@qq.com>
2019-07-28 11:02:21 +0800

    测试:当前分支落后主分支 --> 在开发分支提交文件

--- for_test/test_behand_merge.py
+++ for_test/test_behand_merge.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
 # this is a test file
 
+def func_test_on_dev():
+    print("this function added on feature/dev branch on 2019年7月28日 10点53分")
+
 if __main__ == "main":
     print("this is a test file one feature/dev branch")
