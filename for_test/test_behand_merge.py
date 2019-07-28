a4c932c4f12d599e1350e91046ffe9473e40d7c2
i75930_win10 <1774104802@qq.com>
2019-07-28 10:59:29 +0800

    测试:当前分支落后主分支 --> 在主分支提交文件

--- for_test/test_behand_merge.py
+++ for_test/test_behand_merge.py
@@ -1,5 +1,9 @@
 # -*- coding: UTF-8 -*-
 # this is a test file
 
+def func_test_on_origin():
+    print("this function added on origin branch on 2019年7月28日 10点58分")
+
+
 if __main__ == "main":
     print("this is a test file one origin branch")
